67430afee55bf784cd1114ee6679242b67ce7e45
k2works <kakimomokuri@gmail.com>
2014-06-14 19:46:55 +0900

    wip

--- smart_reception.rb
+++ smart_reception.rb
@@ -3,14 +3,18 @@ class SmartReception < Sinatra::Base
   set :public_folder => "public", :static => true
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
   get "/index" do
     erb :index
   end
 
-  get "/index2" do
-    erb :index2
+  get "/ok" do
+    erb :thanks
+  end
+
+  get "/ng" do
+    erb :thanks
   end
 end
